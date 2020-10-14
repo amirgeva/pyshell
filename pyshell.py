3aa1834c3a2c81a84895d5b75e7b93c6fb540839
Amir Geva <amir.geva@r-go.io>
2020-10-14 19:23:34 +0300

    change to python3

--- pyshell.py
+++ pyshell.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import sys
 import subprocess
 import os
@@ -28,7 +28,7 @@ def runcmd(cmd):
         try:
             os.chdir(dir)
         except OSError:
-            print 'No such directory'
+            print('No such directory')
         return ''
     else:
         dbg("===: '{}'".format(cmd))
@@ -66,15 +66,15 @@ def process_escapes(cmd):
 
 def run_python_cmd(cmd):
     try:
-        exec cmd in user_vars
-    except AttributeError,e:
-        print e
-    except TypeError,e:
-        print e
-    except IndentationError,e:
-        print e
-    except IndexError,e:
-        print e
+        exec (cmd, user_vars)
+    except AttributeError as e:
+        print(e)
+    except TypeError as e:
+        print(e)
+    except IndentationError as e:
+        print(e)
+    except IndexError as e:
+        print(e)
 
 
 def main():
@@ -91,6 +91,7 @@ def main():
         set_colors('blue','black',True)
         sys.stdout.write('{}>'.format(prompt));
         set_colors('green','black',False)
+        sys.stdout.flush()
         cmd=sys.stdin.readline().rstrip()
         #cmd=process_escapes(cmd)
         cmd=process_back_quotes(cmd)
@@ -108,12 +109,12 @@ def main():
         try:
             dbg("Trying '{}' type={}".format(cmd,type(cmd)))
             run_python_cmd(cmd)
-        except NameError,e:
+        except NameError as e:
             msg=str(e)
             dbg("NameError: '{}'".format(msg))
             res=runcmd(cmd)
             sys.stdout.write(res)
-        except SyntaxError,e:
+        except SyntaxError as e:
             msg=str(e)
             dbg("SyntaxError: '{}'".format(msg))
             if msg.find('unexpected EOF while parsing')>=0:
